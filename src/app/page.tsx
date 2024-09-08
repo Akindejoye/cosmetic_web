9b0caacc0188243d0d10a9c8e3c20551c0b878b8
Akindejoye <tydruhill@gmail.com>
2024-09-08 21:56:43 +0100

    Product Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import NewArrival from "@/components/NewArrival";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      <NewArrival />
     </div>
   );
 }
